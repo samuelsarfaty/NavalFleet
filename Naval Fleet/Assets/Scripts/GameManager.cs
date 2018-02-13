50ad1a187de4e1c302dcb8d8aa434887c5813402
Samuel Sarfaty <samuel.sarfaty@gmail.com>
2018-02-13 14:50:46 +0000

    added win and lose scenes, started changing money ui

--- Naval Fleet/Assets/Scripts/GameManager.cs
+++ Naval Fleet/Assets/Scripts/GameManager.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
 	public Text moneyText;
 	public static int coins;
 	public int startCoins;
+
+	private LevelManager levelManager;
+
+	void Awake(){
+		levelManager = GameObject.FindObjectOfType<LevelManager> ();
+	}
 
 	// Use this for initialization
 	void Start () {
