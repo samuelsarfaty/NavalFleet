46c8c746bf2b001580c2b9e640aee1aebea7bcd6
Samuel Sarfaty <samuel.sarfaty@gmail.com>
2018-02-14 18:19:55 +0000

    Added icons for uints

--- Naval Fleet/Assets/Scripts/Mothership.cs
+++ Naval Fleet/Assets/Scripts/Mothership.cs
@@ -7,12 +7,15 @@ public class Mothership : MonoBehaviour {
 	public float delayBetweenExplosions;
 
 	public GameObject deathExplosion;
+	public AudioClip deathExplosionSound;
 	public GameObject explosionLocation_1, explosionLocation_2, explosionLocation_3; 
 
 	private LevelManager lm;
+	private AudioSource source;
 
 	void Awake(){
 		lm = GameObject.FindObjectOfType<LevelManager> ();
+		source = GetComponent<AudioSource> ();
 	}
 
 	IEnumerator MothershipDeath(){ //Spawm 3 explosions and then load the "Lose" scene
@@ -21,7 +24,8 @@ public class Mothership : MonoBehaviour {
 		Instantiate (deathExplosion, explosionLocation_2.transform.position, Quaternion.identity);
 		yield return new WaitForSeconds (delayBetweenExplosions);
 		Instantiate (deathExplosion, explosionLocation_3.transform.position, Quaternion.identity);
-		yield return new WaitForSeconds (1);
+		source.PlayOneShot (deathExplosionSound);
+		yield return new WaitForSeconds (3);
 		lm.LoadLevel ("Lose");
 	}
 
