7a30bd143a306e657400f8b5988f7ceae2b1f765
Samuel Sarfaty <samuel.sarfaty@gmail.com>
2018-02-15 15:08:47 +0000

    Added coin spawner

---
+++ Naval Fleet/Assets/Scripts/CoinSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpawner : MonoBehaviour {
+
+	public GameObject coinPrefab;
+	public float spawnRate;
+
+	public float minX, maxX, minY, maxY;
+
+	void Start(){
+		InvokeRepeating ("SpawnCoin", spawnRate, spawnRate);
+	}
+
+	void SpawnCoin(){
+		float xPos = Random.Range (minX, maxX);
+		float yPos = Random.Range (minY, maxY);
+
+		Instantiate (coinPrefab, new Vector2 (xPos, yPos), Quaternion.identity);
+	}
+}
